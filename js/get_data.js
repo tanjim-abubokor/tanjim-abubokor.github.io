720a38150899eb785ccf943fc9ef1820fb9514a2
Tanjim Abubokor <153937362+tanjim-abubokor@users.noreply.github.com>
2023-12-17 23:42:25 +0600

    Update get_data.js

--- js/get_data.js
+++ js/get_data.js
@@ -461,7 +461,7 @@ function BlogDetails(){
             <div class="news_popup_informations">
                 <div class="image" style="margin-bottom: 20px;">
                     <img src="img/thumbs/4-2.jpg" alt="">
-                    <img class="main" src="${blog.image}" style="max-height: 350px; display:block;  max-width:fit-content;">
+                    <img class="popup_image" src="${blog.image}" style="max-height: 350px; display:block;  max-width:fit-content;">
                 </div>
                 <div class="details">
                     <div class="meta">
